c7c6de5b1e6ecc7c8d001fafd8cee701a0bd8921
Angga Ari Wijaya <anggadarkprince@gmail.com>
2016-10-26 19:18:38 +0700

    set default task and add gulp utility

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@
 
 // include gulp and plugins
 var gulp = require('gulp');
+var util = require('gulp-util');
 var newer = require('gulp-newer');
 var imagemin = require('gulp-imagemin');
 var del = require('del');
@@ -15,21 +16,21 @@ var images = {
 }
 
 // clean the build folder
-gulp.task('clean', function(){
+gulp.task('clean', function () {
     del([
         dest + '*'
     ]);
 })
 
 // manage images
-gulp.task('images', function(){
+gulp.task('images', function () {
     return gulp.src(images.in)
         .pipe(newer(images.out))
         .pipe(imagemin())
         .pipe(gulp.dest(images.out))
 });
 
-// default task
-gulp.task('default', function(){
-
+// default task, task run in almost same time not by order of array
+gulp.task('default', ['images'], function () {
+    util.log('Run default gulp task');
 });
