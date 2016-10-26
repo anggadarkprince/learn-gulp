d498f7f788eaa54c4bb7859a16015abd04f6f8b4
Angga Ari Wijaya <anggadarkprince@gmail.com>
2016-10-26 19:37:17 +0700

    set build type

--- gulpfile.js
+++ gulpfile.js
@@ -6,14 +6,20 @@ var util = require('gulp-util');
 var newer = require('gulp-newer');
 var imagemin = require('gulp-imagemin');
 var del = require('del');
+pkg = require('./package.json');
 
 // file locations
+// add constant variable on linux via terminal `export NODE_ENV=production` check echo $NODE_ENV
+var devBuild = ((process.env.NODE_ENV || 'development').trim().toLowerCase() !== 'production')
 var source = 'src/'
 var dest = 'build/';
 var images = {
     in: source + 'images/*.*',
     out: dest + 'images/'
 }
+
+// show build type
+console.log(pkg.name + ' ' + pkg.version + ', ' + (devBuild ? 'development' : 'production') + ' build');
 
 // clean the build folder
 gulp.task('clean', function () {
