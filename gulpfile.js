505eddcaed27478c791b75f1988d7156cbf4d274
Angga Ari Wijaya <anggadarkprince@gmail.com>
2016-10-26 19:03:58 +0700

    add image compressor and check newer task

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,24 @@
 
 // include gulp and plugins
 var gulp = require('gulp');
+var newer = require('gulp-newer');
+var imagemin = require('gulp-imagemin');
 
 // file locations
 var source = 'src/'
 var dest = 'build/';
+var images = {
+    in: source + 'images/*.*',
+    out: dest + 'images/'
+}
+
+// manage images
+gulp.task('images', function(){
+    return gulp.src(images.in)
+        .pipe(newer(images.out))
+        .pipe(imagemin())
+        .pipe(gulp.dest(images.out))
+});
 
 // default task
 gulp.task('default', function(){
